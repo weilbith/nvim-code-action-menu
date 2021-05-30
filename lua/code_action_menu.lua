3095eba488a42c7ad91854a1c8596d912b777a80
Thore Strassburg <thore@weilbier.net>
2021-05-30 20:08:55 +0200

    Improve class instance handling

--- lua/code_action_menu.lua
+++ lua/code_action_menu.lua
@@ -1,29 +1,41 @@
 local lsp_requests = require('code_action_menu.lsp_requests')
-local menu_window = require('code_action_menu.menu_window'):new()
-local details_window = require('code_action_menu.details_window'):new()
+local MenuWindow = require('code_action_menu.menu_window')
+local DetailsWindow = require('code_action_menu.details_window')
+
+local menu_window_instance = nil
+local details_window_instance = nil
 
 local function open_code_action_menu()
   local all_code_actions = lsp_requests.request_code_actions()
 
   if all_code_actions ~= nil then
-    menu_window:open(all_code_actions)
+    if menu_window_instance == nil then
+      menu_window_instance = MenuWindow:new()
+    end
+
+    menu_window_instance:open(all_code_actions)
   end
 end
 
+local function update_code_action_details()
+  local code_action = menu_window_instance:get_selected_code_action()
+
+  if details_window_instance == nil then
+    details_window_instance = DetailsWindow:new()
+  end
+
+  details_window_instance:open_or_update(code_action, menu_window_instance.window_number)
+end
+
 local function execute_selected_code_action()
-  local selected_code_action = menu_window:get_selected_code_action()
-  menu_window:close() -- Close first to execute the action in the correct buffer.
+  local selected_code_action = menu_window_instance:get_selected_code_action()
+  menu_window_instance:close() -- Close first to execute the action in the correct buffer.
   lsp_requests.execute_code_action(selected_code_action)
 end
 
 local function close_code_action_menu()
-  details_window:close()
-  menu_window:close()
-end
-
-local function update_code_action_details()
-  local code_action = menu_window:get_selected_code_action()
-  details_window:open_or_update(code_action, menu_window.window_number)
+  details_window_instance:close()
+  menu_window_instance:close()
 end
 
 return {
